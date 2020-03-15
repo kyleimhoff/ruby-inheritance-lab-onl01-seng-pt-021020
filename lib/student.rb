25de3f44132a8a90572d365ef3f55c889a859a7b
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-03-15 17:37:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,12 +1,12 @@
 class Student < User 
   
   
-  def initialize(knowledge)
+  def initialize
     @knowledge = []
   end
   
-  def learn(string)
-    @knowledge << string
+  def learn(knowledge)
+    @knowledge << knowledge
   end
   
   def knowledge 
